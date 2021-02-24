11b9ec59b4a119b62366320933df7854e4eb6c35
Lubeda90 <lubeda@miumg.edu.gt>
2021-02-24 15:03:52 -0600

    Carga final de calculadora

--- Calculadora/src/main/java/com/mycompany/calculadora/Principal.java
+++ Calculadora/src/main/java/com/mycompany/calculadora/Principal.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author alumno
+ * @author lubeda
  */
 public class Principal {
     
@@ -17,12 +17,15 @@ public class Principal {
     public static void main(String[] args) {
         //Definir variables
         Scanner objInput = new Scanner(System.in);
+        Scanner objInput2 = new Scanner(System.in);
         String strContinuar = null;
         
         do {
             System.out.println("Seleccionar una opcion:");
             System.out.println("a - Si desea sumar");
             System.out.println("b - Si desea restar");
+            System.out.println("c - Si desea multiplicar");
+            System.out.println("d - Si desea dividir");
             
             //Escribe el valor del buffer de teclado
             System.out.println("Eliga su opcion:");
@@ -44,10 +47,10 @@ public class Principal {
                     for (int i=0; i<2; i++) {
                         System.out.println("Ingrese el numero " + (i+1) + " : ");
                         fltArrNumeros[i] = objInput.nextFloat();
+                        objInput.nextLine(); //ejecuta la siguiente linea, nextFloat no lo hace
                     }
                     
                     fltResultado = fltArrNumeros[0] + fltArrNumeros[1];
-                    
                     //Control
                     blnError = false;
                     
@@ -61,16 +64,54 @@ public class Principal {
                     for (int i=0; i<2; i++) {
                         System.out.println("Ingrese el numero " + (i+1) + " : ");
                         fltArrNumeros[i] = objInput.nextFloat();
+                        objInput.nextLine(); //ejecuta la siguiente linea, nextFloat no lo hace
                     }
                     
                     fltResultado = fltArrNumeros[0] - fltArrNumeros[1];
-                    
                     //Control
                     blnError = false;
                     
                     //Sale del case
                     break;
+                case "c":
+                   strOpcion = "Multiplicacion";
+                    System.out.println("");
+
+                    //solicitar valores numericos
+                    for (int i=0; i<2; i++) {
+                        System.out.println("Ingrese el numero " + (i+1) + " : ");
+                        fltArrNumeros[i] = objInput.nextFloat();
+                        objInput.nextLine(); //ejecuta la siguiente linea, nextFloat no lo hace
+                    }
                     
+                    fltResultado = fltArrNumeros[0] * fltArrNumeros[1];
+                    //Control
+                    blnError = false;
+                    
+                    //Sale del case
+                    break;
+                 case "d":
+                   strOpcion = "Division";
+                    System.out.println("");
+
+                    //solicitar valores numericos
+                    for (int i=0; i<2; i++) {
+                        System.out.println("Ingrese el numero " + (i+1) + " : ");
+                        fltArrNumeros[i] = objInput.nextFloat();
+                        objInput.nextLine(); //ejecuta la siguiente linea, nextFloat no lo hace
+                    }
+                    if (fltArrNumeros[1] == 0) { //validar division entre 0
+                        System.out.println("La division entre 0 no es correcta");
+                        //Control
+                        blnError = true;   
+                    }else{
+                        fltResultado = fltArrNumeros[0] / fltArrNumeros[1];
+                        //Control
+                        blnError = false;
+                    }
+                    
+                    //Sale del case
+                    break;            
                 default:
                     blnError = true;
                     break;
@@ -89,7 +130,6 @@ public class Principal {
             
             System.out.println("Desea continuar [S/N]: ");
             //Capturar el buffer para continuar
-            Scanner objInput2 = new Scanner(System.in);
             strContinuar = objInput2.nextLine();
         }while(strContinuar.equals("s") || strContinuar.equals("S"));               
     }
